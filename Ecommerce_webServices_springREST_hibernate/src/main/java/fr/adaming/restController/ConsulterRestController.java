0ceff2fcdb5592461cd7b6e634ef7221596f8d2b
Tchement <tchement.enseirb@gmail.com>
2016-11-18 14:59:39 +0100

    ConsulterRestController modifié

--- Ecommerce_webServices_springREST_hibernate/src/main/java/fr/adaming/restController/ConsulterRestController.java
+++ Ecommerce_webServices_springREST_hibernate/src/main/java/fr/adaming/restController/ConsulterRestController.java
@@ -7,59 +7,70 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import fr.adaming.model.Categorie;
-import fr.adaming.model.Commande;
 import fr.adaming.model.Produit;
 import fr.adaming.service.IConsulterService;
 
-
 @RestController
 public class ConsulterRestController {
-	
-@Autowired
-@Qualifier(value="ConsulterServiceImpl")
-IConsulterService consulterService;
 
+	@Autowired
+	@Qualifier(value = "ConsulterServiceImpl")
+	IConsulterService consulterService;
 
+	// -----------------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------------
+	/**
+	 * Méthodes du web-services
+	 */
 
+	/**
+	 * Rechercher tous les produits
+	 */
+	@RequestMapping(value = "/getAllProduits", method = RequestMethod.GET, produces = "application/json")
+	protected List<Produit> consulterTousLesProduits() {
+		return consulterService.consulterTousLesProduits();
+	}
 
+	/**
+	 * Rechercher toutes les catégories
+	 */
+	@RequestMapping(value = "/getAllCategories", method = RequestMethod.GET, produces = "application/json")
+	protected List<Categorie> consulterToutesLesCategories() {
+		return consulterService.consulterToutesLesCategories();
+	}
 
+	/**
+	 * Rechercher tous les produits d'une catégorie
+	 */
+	@RequestMapping(value = "/getAllProduitsByCategorie/{id_categ}", method = RequestMethod.GET, produces = "application/json")
+	protected List<Produit> consulterLesProduitsParCategorie(@PathVariable("id_categ") int id_categ) {
+		return consulterService.consulterLesProduitsParCategorie(id_categ);
+	}
 
-@RequestMapping(value="/allProduits", method=RequestMethod.GET, produces="application/json")
- protected List<Produit> consulterTousLesProduits() {
-	
-	return consulterService.consulterTousLesProduits();
-}
+	/**
+	 * Rechercher un produit par son ID
+	 */
+	@RequestMapping(value = "/getProduitById/{id_produit}", method = RequestMethod.GET, produces = "application/json")
+	protected Produit consulterProduitSelectionne(@PathVariable("id_produit") int id_produit) {
+		return consulterService.consulterProduitSelectionne(id_produit);
+	}
 
-@RequestMapping(value="/allCategories", method=RequestMethod.GET, produces="application/json")
-protected List<Categorie> consulterToutesLesCategories() {
-	return consulterService.consulterToutesLesCategories();
-}
+	/**
+	 * Rechercher un produit par mot-clef
+	 */
+	@RequestMapping(value = "/ProduitsBySearch/{motCle}", method = RequestMethod.GET, produces = "application/json")
+	protected List<Produit> chercherProduitsParMotCle(@PathVariable("motCle") String motCle) {
+		return consulterService.chercherProduitsParMotCle(motCle);
+	}
 
-@RequestMapping(value="/allProduitsByCategorie/{id_categ}", method=RequestMethod.GET, produces="application/json")
-protected List<Produit> consulterLesProduitsParCategorie(@PathVariable("id_categ") int id_categ) {
-	
-	return consulterService.consulterLesProduitsParCategorie(id_categ);
-}
-
-@RequestMapping(value="/getProduit/{id_produit}", method=RequestMethod.GET, produces="application/json")
-protected Produit consulterProduitSelectionne(@PathVariable("id_produit") int id_produit) {
-	
-	return consulterService.consulterProduitSelectionne(id_produit);
-}
-
-@RequestMapping(value="/ProduitsBySearch/{motCle}", method=RequestMethod.GET, produces="application/json")
-protected List<Produit> chercherProduitsParMotCle(@PathVariable("motCle") String motCle) {
-	return consulterService.chercherProduitsParMotCle(motCle);
-}
-
-@RequestMapping(value="/getCategorie/{id_categorie}", method=RequestMethod.GET, produces="application/json")
-protected Categorie consulterCategorieParId(@PathVariable("id_categorie") int id_categorie) {
-	return consulterService.consulterCategorieParId(id_categorie);
-}
-
-
+	/**
+	 * Rechercher une catégorie par son ID
+	 */
+	@RequestMapping(value = "/getCategorie/{id_categorie}", method = RequestMethod.GET, produces = "application/json")
+	protected Categorie consulterCategorieParId(@PathVariable("id_categorie") int id_categorie) {
+		return consulterService.consulterCategorieParId(id_categorie);
+	}
 
 }
